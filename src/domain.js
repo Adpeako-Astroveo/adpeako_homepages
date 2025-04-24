34e79dffd6bc314000d14ffec31711792aef9fa7
eric valdez <37236211+ericgoodkid@users.noreply.github.com>
2025-04-24 21:59:54 +0800

    Commit from Bolt to GitHub

--- src/domain.js
+++ src/domain.js
@@ -4,6 +4,9 @@ import v3Design from './designs/v3.js';
 import localhostDesign from './designs/localhost.js';
 
 export default {
+  "jazzy-mango.com": "design_v1",
+  "brisk-otter.com": "design_v2",
+  "brisk-elm.com": "design_v3",
   "localhost": "design_v1",
   "127.0.0.1": "design_v1"
 };
