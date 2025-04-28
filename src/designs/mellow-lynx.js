0516ae50e5fee3d3a62aa1944d0346095ccab2d3
eric valdez <37236211+ericgoodkid@users.noreply.github.com>
2025-04-28 14:57:32 +0800

    Commit from Bolt to GitHub

---
+++ src/designs/mellow-lynx.js
@@ -0,0 +1,27 @@
+export default {
+  id: 'mellow-lynx.com',
+  name: 'Mellow Lynx Design',
+  theme: {
+    primary: '#FF6B6B',
+    secondary: '#4ECDC4'
+  },
+  pages: {
+    home: {
+      title: 'Your Domain | Home',
+      description: 'Your ultimate mobile entertainment destination. Stream music, watch videos, follow sports, play games, and listen to audiobooks - all with simple mobile billing.',
+      content: 'Premium mobile entertainment at your fingertips.',
+      component: () => import('../pages/HomePage.vue')
+    },
+    privacy: {
+      title: 'Your Domain | Privacy',
+      description: 'Learn about our commitment to protecting your privacy and personal data while using our entertainment services.',
+      content: 'Our privacy commitment to you.',
+      component: () => import('../pages/PrivacyPage.vue')
+    },
+    notFound: {
+      title: 'Your Domain | Page Not Found',
+      description: 'The page you are looking for could not be found.',
+      content: 'Sorry, this page does not exist'
+    }
+  }
+};
--- src/domain.js
+++ src/domain.js
@@ -2,6 +2,7 @@ import jazzyMangoDesign from './designs/jazzy-mango.js';
 import briskOtterDesign from './designs/brisk-otter.js';
 import briskElmDesign from './designs/brisk-elm.js';
 import gleamingFernDesign from './designs/gleaming-fern.js';
+import mellowLynxDesign from './designs/mellow-lynx.js';
 import localhostDesign from './designs/localhost.js';
 import defaultDesign from './designs/default.js';
 
@@ -10,6 +11,7 @@ export default {
   "brisk-otter.com": "brisk-otter.com",
   "brisk-elm.com": "brisk-elm.com",
   "gleaming-fern.com": "gleaming-fern.com",
+  "mellow-lynx.com": "mellow-lynx.com",
   "localhost": "jazzy-mango.com",
   "127.0.0.1": "jazzy-mango.com"
 };
@@ -19,6 +21,7 @@ export const designs = {
   "brisk-otter.com": briskOtterDesign,
   "brisk-elm.com": briskElmDesign,
   "gleaming-fern.com": gleamingFernDesign,
+  "mellow-lynx.com": mellowLynxDesign,
   "design_local001": localhostDesign,
   "default": defaultDesign
 };
