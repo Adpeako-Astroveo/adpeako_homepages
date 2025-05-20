99722aeb116a067ebc114de2cbf3c7e4e817598c
Towsiff <towsiff333@gmail.com>
2025-05-20 21:55:24 +0600

    manual push

--- src/designs/nextlevelstreams/PrivacyPage.vue
+++ src/designs/nextlevelstreams/PrivacyPage.vue
@@ -2,7 +2,7 @@
   <div class="privacy-page">
     <div class="privacy-container">
       <h1>Privacy Policy</h1>
-      <p class="last-updated">Last updated: June 15, 2025</p>
+      <p class="last-updated">Last updated: June 11, 2024</p>
 
       <section>
         <h2>Introduction</h2>
